1fca4947657e521ae67cca770a6fc893c1e0cc23
amanuelHT <aman3hda@gmail.com>
2025-04-15 05:37:28 +0200

    almost done backend and front

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,19 @@ module.exports = {
           '0%': { opacity: 0, transform: 'translateY(20px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
         },
+        typing: {
+          '0%': { width: '0%' },
+          '50%': { width: '100%' },
+          '100%': { width: '0%' },
+        },
+        blink: {
+          '0%, 100%': { borderColor: 'transparent' },
+          '50%': { borderColor: '#f6e8d7' },
+        },
       },
       animation: {
         'fade-in': 'fadeIn 1s ease-out forwards',
+        'typing': 'typing 25s steps(40, end) infinite, blink .75s step-end infinite',
       },
     },
   },
